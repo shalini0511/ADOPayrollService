ee4902a6d00eaa8d9f1f18e47e84d0a10d011ecb
shalini <shalini.v@tvsnext.io>
2021-08-03 21:24:36 +0530

    [shalini]initial commit

---
+++ ADO_Employee_Payroll/ADO_Employee_Payroll/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ADO_Employee_Payroll
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Payroll Services using ADO!");
+        }
+    }
+}
