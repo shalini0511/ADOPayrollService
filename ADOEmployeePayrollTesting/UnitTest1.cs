50ce9ef3dc6fe9460572aa32b2a3f3decb41106a
shalini <shalini.v@tvsnext.io>
2021-08-04 14:48:38 +0530

    [shalini]AggregateFunction

--- ADOEmployeePayrollTesting/UnitTest1.cs
+++ ADOEmployeePayrollTesting/UnitTest1.cs
@@ -5,7 +5,6 @@ using ADO_Employee_Payroll;
 
 namespace ADOEmployeePayrollTesting
 {
-
     [TestClass]
     public class PayrollServiceTesting
     {
@@ -41,7 +40,7 @@ namespace ADOEmployeePayrollTesting
 
         //Usecase 4: Update basic pay in Sql Server using Stored Procedure
         [TestMethod]
-        [TestCategory("Using Stored Procedure")]
+        [TestCategory("Using sql query")]
         public void GivenSelectQuery_UsingStoredProcedure_ReturnTwo()
         {
             EmployeeDataManager employeeDataManager = new EmployeeDataManager();
@@ -51,14 +50,36 @@ namespace ADOEmployeePayrollTesting
             Assert.AreEqual(actual, expected);
         }
 
-        //Usecase 4: Update basic pay in Sql Server using Stored Procedure
+        //Usecase 5: Update basic pay in Sql Server using Stored Procedure
         [TestMethod]
-        [TestCategory("Using Stored Procedure")]
+        [TestCategory("Using sql query")]
         public void GivenStartDate_UsingStoredProcedure_ReturnStringodName()
         {
             EmployeeDataManager employeeDataManager = new EmployeeDataManager();
             string expected = "Harsha Varghese Ashaya Sivakumar ";
             string actual = employeeRepository.DataBasedOnDateRange();
+            Assert.AreEqual(actual, expected);
+        }
+
+        //Usecase 6: Aggregate Functions
+        [TestMethod]
+        [TestCategory("Using SQL Query for Female")]
+        public void GivenGenderFemale_GroupBygender_ReturnAggregateFunction()
+        {
+            EmployeeDataManager employeeDataManager = new EmployeeDataManager();
+            string expected = "3069000 19000 3000000 1023000 3";
+            string query = "select sum(BasicPay) as TotalSalary,min(BasicPay) as MinimumSalary,max(BasicPay) as MaximumSalary,Round(avg(BasicPay), 0) as AverageSalary,Count(BasicPay) as Count from employee_payroll where Gender = 'F' group by Gender";
+            string actual = employeeRepository.AggregateFunctionBasedOnGender(query);
+            Assert.AreEqual(actual, expected);
+        }
+        [TestMethod]
+        [TestCategory("Using SQL Query for Male")]
+        public void GivenGenderMale_GroupBygender_ReturnAggregateFunction()
+        {
+            EmployeeDataManager employeeDataManager = new EmployeeDataManager();
+            string expected = "30250000 250000 30000000 15125000 2";
+            string query = "select sum(BasicPay) as TotalSalary,min(BasicPay) as MinimumSalary,max(BasicPay) as MaximumSalary,Round(avg(BasicPay), 0) as AverageSalary,Count(BasicPay) as Count from employee_payroll where Gender = 'M' group by Gender";
+            string actual = employeeRepository.AggregateFunctionBasedOnGender(query);
             Assert.AreEqual(actual, expected);
         }
     }
