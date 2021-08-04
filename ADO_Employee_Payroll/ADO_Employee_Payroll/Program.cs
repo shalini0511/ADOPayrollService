bc045792e327a39f7eab99d7cd2027a4ef356795
shalini <shalini.v@tvsnext.io>
2021-08-04 12:43:11 +0530

    [shalini]Retrieve data from database

--- ADO_Employee_Payroll/ADO_Employee_Payroll/Program.cs
+++ ADO_Employee_Payroll/ADO_Employee_Payroll/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using ADO_Employee_Payroll.ADO_Employee_Payroll;
 
 namespace ADO_Employee_Payroll
 {
@@ -7,6 +8,10 @@ namespace ADO_Employee_Payroll
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Payroll Services using ADO!");
+            //Create oobject for Employee Repository
+            EmployeeRepository employeeRepository = new EmployeeRepository();
+            employeeRepository.GetSqlData();
+
         }
     }
 }
